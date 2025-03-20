f6c9a82f7d8254d7324e17f98d818ea27a754016
robogamer9849 <tahanorouzi415@gmail.com>
2025-03-20 16:43:01 +0330

    made the basic ui of the app

---
+++ ui_home.py
@@ -0,0 +1,33 @@
+import kivy 
+from kivy.app import App 
+from kivy.uix.label import Label 
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+
+class fristapp(App):
+    def build(self):
+        mainBox = BoxLayout(orientation = 'vertical')
+        buttonsBox = BoxLayout(orientation = 'horizontal')
+
+        btnHost = Button(
+                text = 'host',
+                font_size = "20sp",
+                background_color = (256, 0 ,100 ,1),
+                size = (32, 32),)
+        btnClient = Button(
+            text = 'client',
+            font_size = "20sp",
+            background_color = (0, 256 ,100 ,1),
+            size = (32, 32),)
+
+        buttonsBox.add_widget(btnHost)
+        buttonsBox.add_widget(btnClient)
+
+        homeText = Label(text = "you want to connetct to a server (client) or be one (host)?")
+
+        mainBox.add_widget(homeText)
+        mainBox.add_widget(buttonsBox)
+
+        return mainBox 
+
+fristapp().run()
